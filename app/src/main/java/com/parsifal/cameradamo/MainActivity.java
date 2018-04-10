eedf12a888c3af3226c25f2aa105996824da33df
wandererparsifal <wandererparsifal@gmail.com>
2018-04-10 10:04:43 +0800

    实现预览功能

--- app/src/main/java/com/parsifal/cameradamo/MainActivity.java
+++ app/src/main/java/com/parsifal/cameradamo/MainActivity.java
@@ -1,13 +1,135 @@
 package com.parsifal.cameradamo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HandlerThread mThreadHandler;
+
+    private Handler mHandler;
+
+    private TextureView mPreviewView;
+
+    private CaptureRequest.Builder mPreviewBuilder;
+
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            try {
+                startPreview(camera);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mThreadHandler = new HandlerThread("CAMERA2");
+        mThreadHandler.start();
+        mHandler = new Handler(mThreadHandler.getLooper());
+        mPreviewView = (TextureView) findViewById(R.id.previewView);
+        mPreviewView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @SuppressLint("MissingPermission")
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+                try {
+                    String[] CameraIdList = cameraManager.getCameraIdList();
+                    //获取可用相机设备列表
+                    CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(CameraIdList[0]);
+                    //在这里可以通过CameraCharacteristics设置相机的功能,当然必须检查是否支持
+                    characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
+                    //就像这样
+                    cameraManager.openCamera(CameraIdList[0], mCameraDeviceStateCallback, mHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        });
     }
+
+    private void startPreview(CameraDevice camera) throws CameraAccessException {
+        SurfaceTexture texture = mPreviewView.getSurfaceTexture();
+        texture.setDefaultBufferSize(mPreviewView.getWidth(), mPreviewView.getHeight());
+        Surface surface = new Surface(texture);
+        try {
+            mPreviewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        mPreviewBuilder.addTarget(surface);
+        camera.createCaptureSession(Arrays.asList(surface), mSessionStateCallback, mHandler);
+    }
+
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            try {
+                //session.capture(mPreviewBuilder.build(), mSessionCaptureCallback, mHandler);
+                session.setRepeatingRequest(mPreviewBuilder.build(), mSessionCaptureCallback, mHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+        }
+    };
+
+    private CameraCaptureSession.CaptureCallback mSessionCaptureCallback =
+            new CameraCaptureSession.CaptureCallback() {
+
+                @Override
+                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request,
+                                               @NonNull TotalCaptureResult result) {
+                }
+
+                @Override
+                public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request,
+                                                @NonNull CaptureResult partialResult) {
+                }
+            };
 }
